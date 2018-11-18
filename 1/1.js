c445ed627cfe69e99dd454c435be2f7e6c463b6c
Syusina Elizaveta <elizaveta.syusina@yandex.ru>
2018-11-19 00:03:03 +0300

    fix

--- 1/1.js
+++ 1/1.js
@@ -1,5 +1,5 @@
-        let str="#";
-        while (str.length < 8){
-          console.log(str);
-          str += "#"
-        }
+let str="#";
+while (str.length < 8) {
+  console.log(str);
+  str += "#";
+}
--- 3/3.js
+++ 3/3.js
@@ -1,11 +1,10 @@
 var N=8;
-        var board="";
-        for (var i = 0; i < N; i++){
-            for (var j = 0; j < N; j++){
-                if ((i+j)%2 == 0)  board+="#"; 
-                else board+=" ";
-               
-            }
-            board += "\n";
-        }
-        console.log(board);
+var board="";
+for (var i = 0; i < N; i++) {
+    for (var j = 0; j < N; j++){
+        if ((i+j)%2 == 0)  board+="#"; 
+        else board+=" ";
+    }
+    board += "\n";
+}
+console.log(board);
